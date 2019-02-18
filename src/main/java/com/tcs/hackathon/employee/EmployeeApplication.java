7709a4881710d7917190aa1fc4e67524880da4ad
Pradipta Nag <pradipta.nag2@gmail.com>
2019-02-18 18:50:36 +0000

    Converting the Spring Boot project to generate a WAR file so that it can be deployed in Tomcat server

--- src/main/java/com/tcs/hackathon/employee/EmployeeApplication.java
+++ src/main/java/com/tcs/hackathon/employee/EmployeeApplication.java
@@ -2,9 +2,10 @@ package com.tcs.hackathon.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeApplication {
+public class EmployeeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
